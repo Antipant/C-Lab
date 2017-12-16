28024ea5e0b51ecec4e81bfaff661f84c62e9ba0
Antipant <31270538+Antipant@users.noreply.github.com>
2017-12-16 22:41:41 +0300

    Add files via upload

---
+++ C_sharp_lab_1/C_sharp_lab_1/Model_aircraft.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace model_aircraft
+{
+    //плюсы
+    //контролируемый доступ к единственному экземпляру.
+    //минусы
+    //глобальные объекты могут быть вредны для объектного программирования, в некоторых случаях приводят к созданию немасштабируемого проекта;  усложняет написание модульных тестов и следование tdd.
+    //применение
+    //должен быть ровно один экземпляр некоторого класса, легко доступный всем клиентам;
+    //единственный экземпляр должен расширяться путем порождения подклассов, и клиентам нужно иметь возможность работать с расширенным экземпляром без модификации своего кода.
+
+    //sealed - от класса нельзя ничего пронаследовать, нельзя использовать protected
+
+    //EventHandler - Представляет метод, который будет обрабатывать событие, не имеющее данных.
+
+
+    public class Singleton
+    {
+        public Singleton() { Console.WriteLine("Model_aircraft"); }
+
+        private static Singleton instance;
+        public static Singleton Instance
+        {
+            get
+            {
+                instance = new Singleton();
+                return instance;
+            }
+        }
+    }
+
+    public class Model_aircraft
+    {
+        public float pitch = 0.0f;
+        public float hight = 0.0f;//угол тангажа
+        public float roll = 0.0f;   //угол крена
+        public float yaw = 0.0f;    //угол рысканья
+
+        //синглтон
+        //1) закрыть конструктор, нельзя создать объект
+        //2) внутри класса создается статический объект
+        //3) функция доступа
+
+        //или атрибут доступа:
+        public float Pitch
+        {
+            set
+            {
+                /*  if ((value >= 10) || (value <= -10))
+                  {
+                      Console.WriteLine("throw new ArgumentOutOfRangeException()");
+                      throw new ArgumentOutOfRangeException();
+                  }*/
+                Console.WriteLine("set");
+                pitch = value;
+                Console.WriteLine("pitch = value");
+            }
+            get
+            {
+                Console.WriteLine("get");
+                Console.WriteLine("return pitch");
+                return pitch;
+            }
+        }
+        public float Speed
+        {
+            set
+            {
+                Console.WriteLine("set");
+                hight = value;
+                Console.WriteLine("hight = value");
+            }
+            get
+            {
+                Console.WriteLine("get");
+                Console.WriteLine("return hight");
+                return hight;
+            }
+        }
+        public float Hight
+        {
+            set;
+            get;
+        }
+
+    }
+
+}
---
+++ C_sharp_lab_1/C_sharp_lab_1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;      //подключаем библиотеки
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using model_aircraft;
+
+namespace C_sharp_lab_1                //пространство имен
+{
+    class Program                      //определяется класс
+    {
+        //определяется статическая(существует только один ее экземпляр, вызывается без создания объекта) функция
+        static void Main(string[] args)           
+        {
+            //В информатике инкапсуляцией(лат.en capsula) называется упаковка данных и/ или функций в единый компонент.
+            //шаг 2
+            //создаем объект класса. объект не проинициализирован
+            // Model_aircraft model_aircraft = null;
+
+            //класс - объект - присваиваем - инициализируем, динамическое создание
+
+            // Model_aircraft model_aircraft = new Model_aircraft();
+
+            Model_aircraft model_aircraft = new Model_aircraft();
+            string param;
+            Console.WriteLine("первый цикл for начат");
+            for (; ; ) //первый цикл for
+            {
+                Console.WriteLine("for(;;)");
+                try
+                {
+                    Console.WriteLine("try");
+                    Console.WriteLine("Insert Pitch");
+                    param = Console.ReadLine();
+                    model_aircraft.Pitch = Convert.ToSingle(param);//setmethod 
+                    Console.WriteLine("break");
+                    break;
+                }
+                catch (FormatException) { Console.WriteLine("catch (FormatException)"); Console.WriteLine("Неверный формат"); }
+                catch (ArgumentOutOfRangeException) { Console.WriteLine("catch (ArgumentOutOfRangeException)"); Console.WriteLine("Не входит в допустимый диапазон"); }
+            }//трассировка
+            Console.WriteLine("первый цикл for завершен");
+            Console.WriteLine("Pitch=" + model_aircraft.Pitch);//getmethod
+
+            Console.WriteLine("втррой цикл for начат");
+            for (; ; ) //второй цикл for
+            {
+                Console.WriteLine("for(;;)");
+                try
+                {
+                    Console.WriteLine("try");
+                    Console.WriteLine("Insert Speed");
+                    param = Console.ReadLine();
+                    model_aircraft.Speed = Convert.ToSingle(param);//setmethod
+                    Console.WriteLine("break");
+                    break;
+                }
+                catch (FormatException) { Console.WriteLine("catch (FormatException)"); Console.WriteLine("Неверный формат"); }
+            }
+            Console.WriteLine("Speed=" + model_aircraft.Speed);//getmethod
+            Console.WriteLine("второй цикл for завершен");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
